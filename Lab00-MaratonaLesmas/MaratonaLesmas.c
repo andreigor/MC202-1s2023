23886046464cf8eb0e055c611262cd571a086961
Andre Igor <andre.igor@pd.griaule>
2023-03-09 18:43:57 -0300

    Estrutura do novo lab01

--- Lab00-MaratonaLesmas/MaratonaLesmas.c
+++ Lab00-MaratonaLesmas/MaratonaLesmas.c
@@ -1,55 +1,49 @@
-/*
-Aluno: André Igor Nóbrega da Silva
-Curso: MC202 - 1s2023 - Unicamp
-Turma: A
-
-
-Descrição: Dada um conjunto de n lesmas, cada uma com sua respectiva velocidade, retornamos a lesma que ganharia uma maratona com uma 
-certa quantidade de horas. Quanto mais rápido uma lesma, menos horas ela é capaz de correr.
-
-*/
-
 #include <stdio.h>
-#define MAX_LESMAS 100
 
-int MaratonaLesmas(int n, int *velocidades, int horas){
-    int limite;
+int main(){
+    /* entradas */
+    /* numero de lesmas:*/
+    int n;
+    scanf("%d",&n);
 
-    if (horas == 1) limite = 100;
-    else if (horas == 2) limite = 20;
-    else if (horas == 3) limite = 10;
-    else return -1; // Valor inválido na quantidade de horas da maratona
-
-    // Maior velocidade das lesmas qualificadas
-
-    int maxima = -1; // inicializando com a velocidade de erro
-
-    for (int i = 0; i < n; i++){
-        if ((velocidades[i] > 0) && (velocidades[i] > maxima) && (velocidades[i] <= limite)) maxima = velocidades[i];
+    /* velocidade das lesmas */
+    int velocidade[100];
+    int i = 0;
+    for(i; i < n; i++){
+    	// printf("i: %d\n", i);
+        scanf("%d",&velocidade[i]);
     }
 
-    return maxima;
-}
-
-int main(void){
-    int n, horas;
-    int velocidades[MAX_LESMAS];
-
-    // Obtendo a quantidade de lesmas
-    scanf("%d", &n);
-
-    // Obtendo as velociadades
-    for (int i = 0; i < n; i++){
-        scanf("%d", &velocidades[i]);
+    /* duracao da maratona */
+    int tempo;
+    scanf("%d",&tempo);
+    
+    int limite= -1;
+    
+    if (tempo == 3){
+        limite = 10;
     }
+    else if (tempo == 2){
+        limite = 20;
+    }
+    else if (tempo == 1){
+        limite == 100;
+    }
+    
+    int ganhadora = -1;
+    
 
-    // Obtendo a duração da maratona
-    scanf("%d", &horas);
-
-    int lesmaGanhadora = MaratonaLesmas(n, velocidades, horas);
-
-    printf("%d", lesmaGanhadora);
-
+    
+    for(i; i < n; i++){
+    	
+        if (velocidade[i] <= limite){
+            if (velocidade[i] > ganhadora){
+                ganhadora = velocidade[i];
+            }
+        }
+    }
+    
+    printf("%d", ganhadora);
     return 0;
-
 }
+
---
+++ Lab00-MaratonaLesmas/MaratonaLesmas_ref.c
@@ -0,0 +1,55 @@
+/*
+Aluno: André Igor Nóbrega da Silva
+Curso: MC202 - 1s2023 - Unicamp
+Turma: A
+
+
+Descrição: Dada um conjunto de n lesmas, cada uma com sua respectiva velocidade, retornamos a lesma que ganharia uma maratona com uma 
+certa quantidade de horas. Quanto mais rápido uma lesma, menos horas ela é capaz de correr.
+
+*/
+
+#include <stdio.h>
+#define MAX_LESMAS 100
+
+int MaratonaLesmas(int n, int *velocidades, int horas){
+    int limite;
+
+    if (horas == 1) limite = 100;
+    else if (horas == 2) limite = 20;
+    else if (horas == 3) limite = 10;
+    else return -1; // Valor inválido na quantidade de horas da maratona
+
+    // Maior velocidade das lesmas qualificadas
+
+    int maxima = -1; // inicializando com a velocidade de erro
+
+    for (int i = 0; i < n; i++){
+        if ((velocidades[i] > 0) && (velocidades[i] > maxima) && (velocidades[i] <= limite)) maxima = velocidades[i];
+    }
+
+    return maxima;
+}
+
+int main(void){
+    int n, horas;
+    int velocidades[MAX_LESMAS];
+
+    // Obtendo a quantidade de lesmas
+    scanf("%d", &n);
+
+    // Obtendo as velociadades
+    for (int i = 0; i < n; i++){
+        scanf("%d", &velocidades[i]);
+    }
+
+    // Obtendo a duração da maratona
+    scanf("%d", &horas);
+
+    int lesmaGanhadora = MaratonaLesmas(n, velocidades, horas);
+
+    printf("%d", lesmaGanhadora);
+
+    return 0;
+
+}
